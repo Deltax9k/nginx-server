7462be3f78e38c9ad2050e25ce40cacb98659ca5
Deltax9k <deltax9000@163.com>
2018-05-18 21:18:44 +0800

    优化代码

--- src/main/java/com/evun/axurerp/FileuploadClient.java
+++ src/main/java/com/evun/axurerp/FileuploadClient.java
@@ -24,177 +24,178 @@ import java.util.concurrent.atomic.AtomicLong;
  * 用于将文件夹压缩后,上传到指定的文件服务器
  */
 public class FileuploadClient {
-    private static final Logger log = LoggerFactory.getLogger(FileuploadClient.class);
+  private static final Logger log = LoggerFactory.getLogger(FileuploadClient.class);
 
-    //文件服务器IP地址,使用jvm启动参数指定,例如: -Dnetty.client.server.ip=10.200.146.119
-    private static final String PARAM_NAME_SERVER_IP = "netty.client.server.ip";
-    //文件服务器端口号,使用jvm启动参数指定,例如: -Dnetty.client.server.port=9360
-    private static final String PARAM_NAME_SERVER_PORT = "netty.client.server.port";
-    //文件需要上传到文件服务器的哪个相对目录下,使用jvm启动参数指定,例如: -Dnetty.client.dirname=9360
-    private static final String PARAM_NAME_DIRNAME = "netty.client.dirname";
-    //需要上传的文件夹绝对路径,使用jvm启动参数指定,例如: -Dnetty.client.localdirpath=/home/admin/pictures
-    private static final String PARAM_NAME_LOCALDIRPATH = "netty.client.localdirpath";
-    private static final String DEFAULT_DIRNAME = "home";
-    private static final String DEFAULT_PORT = "9360";
+  //文件服务器IP地址,使用jvm启动参数指定,例如: -Dnetty.client.server.ip=10.200.146.119
+  private static final String PARAM_NAME_SERVER_IP = "netty.client.server.ip";
+  //文件服务器端口号,使用jvm启动参数指定,例如: -Dnetty.client.server.port=9360
+  private static final String PARAM_NAME_SERVER_PORT = "netty.client.server.port";
+  //文件需要上传到文件服务器的哪个相对目录下,使用jvm启动参数指定,例如: -Dnetty.client.dirname=docs
+  private static final String PARAM_NAME_DIRNAME = "netty.client.dirname";
+  //需要上传的文件夹绝对路径,使用jvm启动参数指定,例如: -Dnetty.client.localdirpath=/home/admin/pictures
+  private static final String PARAM_NAME_LOCALDIRPATH = "netty.client.localdirpath";
+  private static final String DEFAULT_DIRNAME = "home";
+  private static final String DEFAULT_PORT = "9360";
 
-    private static volatile boolean uploadSuccess = false;
+  private static volatile boolean uploadSuccess = false;
 
-    public static void main(String[] args) throws Exception {
-        String localdirpath = System.getProperty(PARAM_NAME_LOCALDIRPATH);
-        if (localdirpath == null || localdirpath.isEmpty()) {
-            log.error("fail to upload, set jvm parameter for local directory, eg: -Dnetty.client.localdirpath=d:/mydir");
-            return;
-        }
-        String serverip = System.getProperty(PARAM_NAME_SERVER_IP);
-        int port = Integer.parseInt(System.getProperty(PARAM_NAME_SERVER_PORT, DEFAULT_PORT));
-        File localdir = new File(localdirpath);
-        String targetDirname = System.getProperty(PARAM_NAME_DIRNAME, DEFAULT_DIRNAME);
-        log.info("uploading local directory: {} to server {}:{}, target home directory: {}",
-                localdir.getAbsolutePath(), serverip, port, targetDirname);
-        upload(serverip, port, localdir, targetDirname);
+  public static void main(String[] args) throws Exception {
+    String localdirpath = System.getProperty(PARAM_NAME_LOCALDIRPATH);
+    if (localdirpath == null || localdirpath.isEmpty()) {
+      log.error("fail to upload, set jvm parameter for local directory, eg: -Dnetty.client.localdirpath=d:/mydir");
+      return;
     }
+    String serverip = System.getProperty(PARAM_NAME_SERVER_IP);
+    int port = Integer.parseInt(System.getProperty(PARAM_NAME_SERVER_PORT, DEFAULT_PORT));
+    File localdir = new File(localdirpath);
+    String targetDirname = System.getProperty(PARAM_NAME_DIRNAME, DEFAULT_DIRNAME);
+    log.info("uploading local directory: {} to server {}:{}, target home directory: {}",
+        localdir.getAbsolutePath(), serverip, port, targetDirname);
+    upload(serverip, port, localdir, targetDirname);
+  }
 
-    /**
-     * 上传指定的目录到指定的文件服务器的工作目录为根目录的某个相对目录下
-     *
-     * @param host      目标文件服务器IP
-     * @param port      目标文件服务器端口
-     * @param uploadDir 要上传的本地文件夹绝对路径
-     * @param targetDir 文件服务的相对路径
-     * @throws Exception
-     */
-    public static void upload(String host, int port,
-                              final File uploadDir, final String targetDir) throws Exception {
-        if (!uploadDir.exists()) {
-            log.error("fail to upload, file does not exists!");
-            return;
-        }
-        if (uploadDir.isFile()) {
-            log.error("fail to upload, single file not supported, upload directory instead!");
-            return;
-        }
-        File temp = uploadDir;
-        final String fileName = uploadDir.getName();
-        if (uploadDir.isDirectory()) {
-            File tempFile = File.createTempFile("temp", ".zip");
-            ZipUtil.zip(uploadDir, tempFile, true);
-            temp = tempFile;
-        }
-        final File uploadFile = temp;
-        EventLoopGroup group = new NioEventLoopGroup();
-        try {
-            Bootstrap b = new Bootstrap();
-            b.group(group).channel(NioSocketChannel.class)
-                    .option(ChannelOption.TCP_NODELAY, true)
-                    .handler(new ChannelInitializer<Channel>() {
-                        @Override
-                        protected void initChannel(Channel ch) throws Exception {
-                            ch.pipeline().addLast(new ObjectEncoder());
-                            ch.pipeline().addLast(new ObjectDecoder(
-                                    ClassResolvers.weakCachingConcurrentResolver(null)));
-                            ch.pipeline().addLast(
-                                    new FileUploadClientHandler(uploadFile, targetDir, fileName));
-                        }
-                    }).connect(host, port).channel().closeFuture().sync();
-        } finally {
-            group.shutdownGracefully();
+  /**
+   * 上传指定的目录到指定的文件服务器的工作目录为根目录的某个相对目录下
+   *
+   * @param host      目标文件服务器IP
+   * @param port      目标文件服务器端口
+   * @param uploadDir 要上传的本地文件夹绝对路径
+   * @param targetDir 文件服务的相对路径
+   * @throws Exception
+   */
+  public static void upload(String host, int port,
+                            final File uploadDir, final String targetDir) throws Exception {
+    if (!uploadDir.exists()) {
+      log.error("fail to upload, file does not exists!");
+      return;
+    } else if (uploadDir.isFile()) {
+      log.error("fail to upload, single file not supported, upload directory instead!");
+      return;
+    }
+    final String fileName = uploadDir.getName();
+    final File uploadFile = File.createTempFile("temp", ".zip");
+    ZipUtil.zip(uploadDir, uploadFile, true);
+    final EventLoopGroup group = new NioEventLoopGroup();
+    new Bootstrap().group(group)
+        .channel(NioSocketChannel.class)
+        .option(ChannelOption.TCP_NODELAY, true)
+        .handler(new ChannelInitializer<Channel>() {
+          @Override
+          protected void initChannel(Channel ch) throws Exception {
+            ch.pipeline().addLast(new ObjectEncoder());
+            ch.pipeline().addLast(new ObjectDecoder(
+                ClassResolvers.weakCachingConcurrentResolver(null)));
+            ch.pipeline().addLast(
+                new FileUploadClientHandler(uploadFile, targetDir, fileName));
+          }
+        }).connect(host, port).channel().closeFuture()
+        .addListener(new GenericFutureListener<Future<? super Void>>() {
+          public void operationComplete(Future<? super Void> future) throws Exception {
             if (uploadSuccess) {
-                boolean rm = IoUtil.rm(uploadDir);
-                if (!rm && uploadDir.exists()) {
-                    log.info("fail to delete directory: {}", uploadFile.getAbsolutePath());
-                }
+              boolean rm = IoUtil.rm(uploadDir);
+              if (!rm && uploadDir.exists()) {
+                log.info("fail to delete directory: {}", uploadFile.getAbsolutePath());
+              }
             }
-        }
+            if (uploadFile.exists() && !IoUtil.rm(uploadFile)) {
+              log.info("fail to delete temporary zip file: {}", uploadFile.getAbsolutePath());
+            }
+            group.shutdownGracefully();
+          }
+        });
+  }
+
+  private static class FileUploadClientHandler
+      extends ChannelInboundHandlerAdapter {
+    private static final int BUFFER_SIZE = 1024 * 8;
+    private final File file;
+    private final String targetDir;
+    private final String fileName;
+    private final AtomicLong fileLength;
+
+    private FileUploadClientHandler(File file, String targetDir, String fileName) {
+      this.file = file;
+      this.targetDir = targetDir;
+      this.fileName = fileName;
+      this.fileLength = new AtomicLong(file.length());
     }
 
-    private static class FileUploadClientHandler
-            extends ChannelInboundHandlerAdapter {
-        private static final int BUFFER_SIZE = 1024 * 8;
-        private final File file;
-        private final String targetDir;
-        private final String fileName;
-        private final AtomicLong fileLength;
-
-        private FileUploadClientHandler(File file, String targetDir, String fileName) {
-            this.file = file;
-            this.targetDir = targetDir;
-            this.fileName = fileName;
-            this.fileLength = new AtomicLong(file.length());
+    public void channelActive(final ChannelHandlerContext ctx) {
+      final long start = System.currentTimeMillis();
+      log.info("file: {} transfer started.", file.getAbsolutePath());
+      InputStream fis = null;
+      try {
+        fis = Files.newInputStream(Paths.get(file.getAbsolutePath()));
+        byte[] bytes;
+        for (int read, position = 0;//read: 已读取的文件字节数, position: 当前总读取字节数
+             (read = fis.read(bytes = new byte[BUFFER_SIZE], 0, BUFFER_SIZE)) != -1;
+             position += read
+            ) {
+          ctx.writeAndFlush(newTransferFile(bytes, read, position))
+              .addListener(newListener(ctx, start, read));
         }
+      } catch (Exception e) {
+        log.error(null, e);
+      } finally {
+        IoUtil.closeQuietly(fis);
+      }
+    }
 
-        public void channelActive(final ChannelHandlerContext ctx) {
-            final long start = System.currentTimeMillis();
-            log.info("file: {} transfer started.", file.getAbsolutePath());
-            InputStream fis = null;
-            try {
-                fis = Files.newInputStream(Paths.get(file.getAbsolutePath()));
-                byte[] bytes;
-                for (int read, position = 0;//read: 已读取的文件字节数, position: 当前总读取字节数
-                     (read = fis.read(bytes = new byte[BUFFER_SIZE], 0, BUFFER_SIZE)) != -1;
-                     position += read
-                        ) {
-                    ctx.writeAndFlush(newTransferFile(bytes, read, position))
-                            .addListener(newListener(ctx, start, read));
-                }
-            } catch (Exception e) {
-                log.error(null, e);
-            } finally {
-                IoUtil.closeQuietly(fis);
-            }
-        }
+    private ChannelFutureListener newListener(
+        final ChannelHandlerContext ctx, final long start, final int currentRead) {
 
-        private ChannelFutureListener newListener(final ChannelHandlerContext ctx, final long start, final int currentRead) {
-            return new ChannelFutureListener() {
-                public void operationComplete(ChannelFuture future) throws Exception {
-                    //计算当前还需要传输的字节数
-                    long length = fileLength.addAndGet(-currentRead);
-                    if (length == 0) {
-                        TransferFile transferFinish = newTransferFinish();
-                        //发送最后的文件传输完成报文
-                        ctx.writeAndFlush(transferFinish).addListener(new GenericFutureListener<Future<? super Void>>() {
-                            public void operationComplete(Future<? super Void> future) throws Exception {
-                                //设置文件传输成功标志位
-                                uploadSuccess = true;
-                                long end = System.currentTimeMillis();
-                                log.info("file: {} transfer finished, time spent: {} ms, speed: {} m/s.",
-                                        file.getAbsolutePath(), end - start,
-                                        (file.length() * 1000 / 1024 / 1024) / (end - start));
-                                if (!file.delete()) {
-                                    log.error("fail to deleted transfered file: {}!", file.getAbsolutePath());
-                                }
-                                ctx.close();
-                            }
-                        });
+      return new ChannelFutureListener() {
+        public void operationComplete(ChannelFuture future) throws Exception {
+          //计算当前还需要传输的字节数
+          long length = fileLength.addAndGet(-currentRead);
+          if (length == 0) {
+            TransferFile transferFinish = newTransferFinish();
+            //发送最后的文件传输完成报文
+            ctx.writeAndFlush(transferFinish)
+                .addListener(new GenericFutureListener<Future<? super Void>>() {
+                  public void operationComplete(Future<? super Void> future) throws Exception {
+                    //设置文件传输成功标志位
+                    uploadSuccess = true;
+                    long end = System.currentTimeMillis();
+                    log.info("file: {} transfer finished, time spent: {} ms, speed: {} m/s.",
+                        file.getAbsolutePath(), end - start,
+                        (file.length() * 1000 / 1024 / 1024) / (end - start));
+                    if (!file.delete()) {
+                      log.error("fail to deleted transfered file: {}!", file.getAbsolutePath());
                     }
-                }
-
-                private TransferFile newTransferFinish() throws InterruptedException {
-                    TransferFile transferFile = new TransferFile();
-                    transferFile.setFilePath(file.getName());
-                    transferFile.setFileName(fileName);
-                    transferFile.setDeleted(false);
-                    //标记文件传输完成
-                    transferFile.setTransferFinished(true);
-                    transferFile.setTargetDirname(targetDir);
-                    return transferFile;
-                }
-            };
+                    ctx.close();
+                  }
+                });
+          }
         }
 
-        private TransferFile newTransferFile(byte[] bytes, int read, int position) {
-            TransferFile transferFile = new TransferFile();
-            transferFile.setFilePath(file.getName());
-            transferFile.setTransferFinished(false);
-            transferFile.setDeleted(false);
-            transferFile.setStartPosition(position);
-            transferFile.setFileBytes(bytes);
-            transferFile.setByteLength(read);
-            return transferFile;
+        private TransferFile newTransferFinish() throws InterruptedException {
+          TransferFile transferFile = new TransferFile();
+          transferFile.setFilePath(file.getName());
+          transferFile.setFileName(fileName);
+          transferFile.setDeleted(false);
+          //标记文件传输完成
+          transferFile.setTransferFinished(true);
+          transferFile.setTargetDirname(targetDir);
+          return transferFile;
         }
-
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
-            log.error(null, cause);
-            ctx.close();
-        }
+      };
     }
+
+    private TransferFile newTransferFile(byte[] bytes, int read, int position) {
+      TransferFile transferFile = new TransferFile();
+      transferFile.setFilePath(file.getName());
+      transferFile.setTransferFinished(false);
+      transferFile.setDeleted(false);
+      transferFile.setStartPosition(position);
+      transferFile.setFileBytes(bytes);
+      transferFile.setByteLength(read);
+      return transferFile;
+    }
+
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) {
+      log.error(null, cause);
+      ctx.close();
+    }
+  }
 }
--- src/main/java/com/evun/axurerp/FileuploadServer.java
+++ src/main/java/com/evun/axurerp/FileuploadServer.java
@@ -22,141 +22,142 @@ import java.io.RandomAccessFile;
  * 将上传的文件解压后,放到指定目录的指定文件夹内
  */
 public class FileuploadServer {
-    private static final Logger log = LoggerFactory.getLogger(FileuploadServer.class);
+  private static final Logger log = LoggerFactory.getLogger(FileuploadServer.class);
 
-    //服务器运行的端口号,使用jvm启动参数指定,例如: -Dnetty.server.port=9360
-    private static final String PARAM_NAME_PORT = "netty.server.port";
-    //服务器的工作目录,所有上传的文件将放在这个目录下, 使用jvm启动参数指定,例如: -Dnnetty.server.home=/home/admin/book
-    private static final String PARAM_NAME_HOME = "netty.server.home";
-    //服务器默认端口号
-    private static final String DEFAULT_PORT = "9360";
-    //服务器默认工作目录,最好使用绝对路径
-    private static final String DEFAULT_HOME = "netty-fileupload-server";
+  //服务器运行的端口号,使用jvm启动参数指定,例如: -Dnetty.server.port=9360
+  private static final String PARAM_NAME_PORT = "netty.server.port";
+  //服务器的工作目录,所有上传的文件将放在这个目录下, 使用jvm启动参数指定,例如: -Dnnetty.server.home=/home/admin/book
+  private static final String PARAM_NAME_HOME = "netty.server.home";
+  //服务器默认端口号
+  private static final String DEFAULT_PORT = "9360";
+  //服务器默认工作目录,最好使用绝对路径
+  private static final String DEFAULT_HOME = "netty-fileupload-server";
 
-    public static void main(String[] args) throws Exception {
-        String portString = System.getProperty(PARAM_NAME_PORT, DEFAULT_PORT);
-        if (portString == null || portString.trim().length() == 0) {
-            throw new IllegalArgumentException("请设置JVM启动参数: -D" + PARAM_NAME_PORT + "=端口号");
-        }
-        int serverPort = Integer.parseInt(portString);
-        final String homeDir = System.getProperty(PARAM_NAME_HOME, DEFAULT_HOME);
-        log.info("文件服务器绑定端口: {}, 工作目录为: {}", serverPort, new File(homeDir).getAbsoluteFile());
-        final NioEventLoopGroup boss = new NioEventLoopGroup();
-        final NioEventLoopGroup worker = new NioEventLoopGroup();
-        new ServerBootstrap()
-                .group(boss, worker)
-                .channel(NioServerSocketChannel.class)
-                .option(ChannelOption.SO_BACKLOG, 1024)
-                .childHandler(new ChannelInitializer<Channel>() {
-                    protected void initChannel(Channel channel) throws Exception {
-                        channel.pipeline()
-                                .addLast(new ObjectEncoder())
-                                .addLast(new ObjectDecoder(Integer.MAX_VALUE,
-                                        ClassResolvers.weakCachingConcurrentResolver(null)))
-                                .addLast(new FileuploadHandler(homeDir));
-                    }
-                })
-                .bind(serverPort)
-                .channel().closeFuture()
-                .addListener(new GenericFutureListener<Future<? super Void>>() {
-                    public void operationComplete(Future<? super Void> future) throws Exception {
-                        boss.shutdownGracefully();
-                        worker.shutdownGracefully();
-                    }
-                });
+  public static void main(String[] args) throws Exception {
+    String portString = System.getProperty(PARAM_NAME_PORT, DEFAULT_PORT);
+    if (portString == null || portString.trim().length() == 0) {
+      throw new IllegalArgumentException("请设置JVM启动参数: -D" + PARAM_NAME_PORT + "=端口号");
+    }
+    final int serverPort = Integer.parseInt(portString);
+    final String homeDir = System.getProperty(PARAM_NAME_HOME, DEFAULT_HOME);
+    final String workingDir = new File(homeDir).getAbsolutePath();
+    final NioEventLoopGroup boss = new NioEventLoopGroup();
+    final NioEventLoopGroup worker = new NioEventLoopGroup();
+    new ServerBootstrap()
+        .group(boss, worker)
+        .channel(NioServerSocketChannel.class)
+        .option(ChannelOption.SO_BACKLOG, 1024)
+        .childHandler(new ChannelInitializer<Channel>() {
+          protected void initChannel(Channel channel) throws Exception {
+            channel.pipeline()
+                .addLast(new ObjectEncoder())
+                .addLast(new ObjectDecoder(Integer.MAX_VALUE,
+                    ClassResolvers.weakCachingConcurrentResolver(null)))
+                .addLast(new FileuploadHandler(homeDir));
+          }
+        })
+        .bind(serverPort)
+        .addListener(new GenericFutureListener<Future<? super Void>>() {
+          public void operationComplete(Future<? super Void> future) throws Exception {
+            log.info("文件服务器启动成功! 绑定端口: {}, 工作目录为: {}", serverPort, workingDir);
+          }
+        })
+        .channel().closeFuture()
+        .addListener(new GenericFutureListener<Future<? super Void>>() {
+          public void operationComplete(Future<? super Void> future) throws Exception {
+            boss.shutdownGracefully();
+            worker.shutdownGracefully();
+            log.info("文件服务器(端口号: {}, 工作目录: {}) 已关闭!", serverPort, workingDir);
+          }
+        });
+  }
+
+  /**
+   * 文件服务器处理逻辑
+   */
+  public static class FileuploadHandler extends ChannelInboundHandlerAdapter {
+    //文件服务器工作目录
+    private final String homeDir;
+
+    FileuploadHandler(String homeDir) {
+      this.homeDir = homeDir;
     }
 
-    /**
-     * 文件服务器处理逻辑
-     */
-    public static class FileuploadHandler extends ChannelInboundHandlerAdapter {
-        //文件服务器工作目录
-        private final String homeDir;
-
-        FileuploadHandler(String homeDir) {
-            this.homeDir = homeDir;
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
+      if (msg != null && msg instanceof TransferFile) {
+        TransferFile transferFile = (TransferFile) msg;
+        File file = new File(homeDir, transferFile.getFilePath());
+        //文件传输完成后,客户端会将transferFinished设置为true
+        if (!transferFile.isTransferFinished()) {
+          //将客户端上传的文件块写入指定文件
+          writeTranferFile(transferFile, file);
+        } else {
+          //将上传完成后的文件移入目标目录中
+          unzipMoveDir(transferFile, file);
         }
-
-        @Override
-        public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-            if (msg != null && msg instanceof TransferFile) {
-                TransferFile transferFile = (TransferFile) msg;
-                File file = new File(homeDir, transferFile.getFilePath());
-                //文件传输完成后,客户端会将transferFinished设置为true
-                if (!transferFile.isTransferFinished()) {
-                    //将客户端上传的文件块写入指定文件
-                    writeTranferFile(transferFile, file);
-                } else {
-                    //将上传完成后的文件移入目标目录中
-                    unzipMoveDir(transferFile, file);
-                }
-            }
-        }
-
-        private void unzipMoveDir(TransferFile transferFile, File file) throws Exception {
-            ZipUtil.unzip(file, new File(homeDir));
-            if (!file.delete()) {
-                log.error("fail to delete file: {}", file.getAbsolutePath());
-            }
-            File unzipDir = new File(homeDir, transferFile.getFileName());
-            if (unzipDir.isDirectory()) {
-                File oldDir = new File(homeDir, transferFile.getTargetDirname());
-                if (oldDir.isDirectory()) {
-                    if (!IoUtil.rm(oldDir)) {
-                        log.error("fail to delete old target home directory: {}", oldDir.getAbsolutePath());
-                    }
-                }
-                if (unzipDir.renameTo(oldDir)) {
-                    log.info("success update home dir: {}", oldDir.getAbsolutePath());
-                } else {
-                    log.error("fail to update home dir: {}", oldDir.getAbsolutePath());
-                }
-            }
-        }
-
-        private void writeTranferFile(TransferFile transferFile, File file) throws IOException {
-            if (deleteIfNecessary(transferFile)) {
-                return;
-            }
-            if (!makeParentDirIfNeccessary(file)) {
-                return;
-            }
-            RandomAccessFile raf = null;
-            try {
-                raf = new RandomAccessFile(file, "rw");
-                raf.seek(transferFile.getStartPosition());
-                raf.write(transferFile.getFileBytes(), 0, transferFile.getByteLength());
-            } finally {
-                IoUtil.closeQuietly(raf);
-            }
-        }
-
-        @Override
-        public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-            log.error(null, cause);
-            ctx.close();
-        }
-
-        private boolean makeParentDirIfNeccessary(File file) {
-            if (!file.exists()) {
-                File parentFile = file.getParentFile();
-                if (!parentFile.isDirectory() && !parentFile.mkdirs()) {
-                    log.info("fail to make parent dirs for file: {}!", file.getAbsoluteFile());
-                    return false;
-                }
-            }
-            return true;
-        }
-
-        private boolean deleteIfNecessary(TransferFile transferFile) {
-            File file = new File(homeDir, transferFile.getFilePath());
-            if (transferFile.isDeleted() && file.exists()) {
-                boolean delete = IoUtil.rm(file);
-                if (!delete) {
-                    log.info("fail to delete file or directory: {}!", file.getAbsoluteFile());
-                }
-            }
-            return transferFile.isDeleted();
-        }
+      }
     }
+
+    private void unzipMoveDir(TransferFile transferFile, File file) throws Exception {
+      ZipUtil.unzip(file, new File(homeDir));
+      if (!file.delete()) {
+        log.error("fail to delete file: {}", file.getAbsolutePath());
+      }
+      File unzipDir = new File(homeDir, transferFile.getFileName());
+      if (unzipDir.isDirectory()) {
+        File oldDir = new File(homeDir, transferFile.getTargetDirname());
+        if (oldDir.exists() && !IoUtil.rm(oldDir)) {
+          log.error("fail to delete old target home directory: {}", oldDir.getAbsolutePath());
+        }
+        if (unzipDir.renameTo(oldDir)) {
+          log.info("success update home dir: {}", oldDir.getAbsolutePath());
+        } else {
+          log.error("fail to update home dir: {}", oldDir.getAbsolutePath());
+        }
+      }
+    }
+
+    private void writeTranferFile(TransferFile transferFile, File file) throws IOException {
+      if (deleteIfNecessary(transferFile)) {
+        return;
+      }
+      if (!makeParentDirIfNeccessary(file)) {
+        return;
+      }
+      RandomAccessFile raf = null;
+      try {
+        raf = new RandomAccessFile(file, "rw");
+        raf.seek(transferFile.getStartPosition());
+        raf.write(transferFile.getFileBytes(), 0, transferFile.getByteLength());
+      } finally {
+        IoUtil.closeQuietly(raf);
+      }
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
+      log.error(null, cause);
+      ctx.close();
+    }
+
+    private boolean makeParentDirIfNeccessary(File file) {
+      if (!file.exists()) {
+        File parentFile = file.getParentFile();
+        if (!parentFile.isDirectory() && !parentFile.mkdirs()) {
+          log.info("fail to make parent dirs for file: {}!", file.getAbsoluteFile());
+          return false;
+        }
+      }
+      return true;
+    }
+
+    private boolean deleteIfNecessary(TransferFile transferFile) {
+      File file = new File(homeDir, transferFile.getFilePath());
+      if (transferFile.isDeleted() && file.exists() && !IoUtil.rm(file)) {
+        log.info("fail to delete file or directory: {}!", file.getAbsoluteFile());
+      }
+      return transferFile.isDeleted();
+    }
+  }
 }
